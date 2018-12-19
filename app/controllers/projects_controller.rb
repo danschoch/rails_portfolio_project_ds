d18656fa8e13c65e69eda95f63d95f0d9eb1e4ec
danschoch <daniel.alexander.schoch@gmail.com>
2018-12-18 18:15:59 -0800

    add basic CRUD actions to relevant controllers

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,29 @@
 class ProjectsController < ApplicationController
+
+    def index
+    end
+    
+    def new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def employee_params
+        params.require().permit
+    end
 end
