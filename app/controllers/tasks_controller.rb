cdc25250360f98aecac75199b8295ce97c396d08
danschoch <daniel.alexander.schoch@gmail.com>
2018-12-19 10:40:04 -0800

    start to build task controller CRUD actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,17 @@
 class TasksController < ApplicationController
+    before_action :set_task, only: [:show, :edit, :update, :destroy]
 
     def index
+        @task = Task.all
     end
     
     def new
+        @task =Task.new
     end
 
     def create
+        @task = Task.create(task_params)
+        redirect_to task_path(@task)
     end
 
     def show
@@ -16,6 +21,8 @@ class TasksController < ApplicationController
     end
 
     def update
+        @task.update(task_params)
+        redirect_to task_params(@task)
     end
 
     def destroy
@@ -23,7 +30,11 @@ class TasksController < ApplicationController
 
     private
 
-    def employee_params
-        params.require().permit
+    def task_params
+        params.require(:task).permit(:content, :lead_notes, :completed, :employee_id, :project_id)
+    end
+
+    def set_task
+        @task = Task.find(params[:id])
     end
 end
