e11a19db7d9b2457b0c17624c61b1051bf1a679f
danschoch <daniel.alexander.schoch@gmail.com>
2018-12-18 19:28:44 -0800

    add instance variable to projects controller actions

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,12 +1,18 @@
 class ProjectsController < ApplicationController
+    before_action :set_project, only: [:show, :edit, :update, :destroy]
 
     def index
+        @projects = Project.all
     end
     
     def new
+        @project = Project.new
     end
 
     def create
+        project = Project.create(project_params)
+        # add current organization to @project.organization_id
+        redirect_to project_path(project)
     end
 
     def show
@@ -16,6 +22,7 @@ class ProjectsController < ApplicationController
     end
 
     def update
+        redirect_to project_path(@project)
     end
 
     def destroy
@@ -23,7 +30,11 @@ class ProjectsController < ApplicationController
 
     private
 
-    def employee_params
-        params.require().permit
+    def project_params
+        params.require(:project).permit(:name, :due_date, :description, :organization_id, :lead_id)
+    end
+
+    def set_project
+        @project = Project.find(params[:id])
     end
 end
