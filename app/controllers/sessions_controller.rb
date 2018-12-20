8ed3c5baa863b28f6fed80988a71fcb5dc19eb00
danschoch <daniel.alexander.schoch@gmail.com>
2018-12-19 19:20:57 -0800

    add login form to view, set up password authentication and set session userid

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,15 @@
 class SessionsController < ApplicationController
-    
+
     def new
         @employee = Employee.find(1)
+    end
+
+    def create
+        @employee = Employee.find_by(email: params[:employee][:email])
+        binding.pry
+        return head(:forbidden) unless @employee.authenticate(params[:employee][:password])
+        session[:employee_id] = @employee.id
+        redirect_to employee_path(@employee)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
   get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   get '/signup', to: 'static_pages#signup'
   
 end
