fcd05ab15ea7c2332d1f9370eecda7839e4cc0a5
danschoch <daniel.alexander.schoch@gmail.com>
2018-12-19 18:03:31 -0800

    create comment form

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
-    belongs_to :task
-    belongs_to :employee
+    belongs_to :task, optional: true
+    belongs_to :employee, optional: true#TODO: delete optional arguments when current_user is available to form
 end
