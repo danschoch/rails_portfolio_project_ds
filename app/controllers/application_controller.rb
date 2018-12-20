bcee96f472f858fb36e245f2c3ca6b6efd66e127
danschoch <daniel.alexander.schoch@gmail.com>
2018-12-19 19:59:55 -0800

    add authentication functionality to relevant controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
     def require_login
-        return head(:forbidden) unless session.include? :user_id
+        return head(:forbidden) unless logged_in?
+    end
+
+    def logged_in?
+        !!session[:employee_id]
+    end
+
+    def current_user
+        Employee.find_by(id: session[:employee_id]) if logged_in?
     end
 end
