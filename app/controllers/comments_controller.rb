e8cc9d2c2898a48be6886e8f63d57eebf0a89412
danschoch <daniel.alexander.schoch@gmail.com>
2018-12-19 18:46:31 -0800

    fix comment form for task_id and add list of comments on task show view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,7 +8,7 @@ class CommentsController < ApplicationController
 
     def create
         comment = Comment.create(comment_params)
-        comment.task_id = params[:task_id]
+        comment[:task_id] = params[:task_id]
         redirect_to task_path(comment.task)
     end
 
@@ -29,4 +29,5 @@ class CommentsController < ApplicationController
     def comment_params
         params.require(:comment).permit(:content, :task_id, :employee_id)
     end
+
 end
