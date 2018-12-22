643cd698b8f3f81463f6bf38c62dd7c46a870da6
danschoch <daniel.alexander.schoch@gmail.com>
2018-12-22 12:29:04 -0800

    add due_date column to tasks table and migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_19_043525) do
+ActiveRecord::Schema.define(version: 2018_12_22_202458) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2018_12_19_043525) do
     t.integer "employee_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "due_date"
   end
 
 end
