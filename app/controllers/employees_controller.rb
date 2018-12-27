d75c93bfc61eb28c4d9e883caa682a8939e17313
danschoch <daniel.alexander.schoch@gmail.com>
2018-12-27 12:07:41 -0800

    update create employee action to handle lead signing up from existing organization

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -2,7 +2,7 @@ class EmployeesController < ApplicationController
     before_action :require_login
     skip_before_action :require_login, only: [:new, :create] #TODO Find a solution to this workaround
     before_action :set_employee, only: [:show, :edit, :update, :destroy]
-    before_action :set_user, only: [:show, :edit]
+    before_action :set_user
 
     def index
         @employees = Employee.all
@@ -15,12 +15,21 @@ class EmployeesController < ApplicationController
 
     def create
         @employee = Employee.create(employee_params)
-        if @employee && @employee.authenticate(params[:employee][:password])
-            session[:employee_id] = @employee.id
-            redirect_to employee_path(@employee) #TODO DRY this code
+        if !session[:employee_id]
+            if @employee && @employee.authenticate(params[:employee][:password])
+                session[:employee_id] = @employee.id
+                redirect_to employee_path(@employee) #TODO DRY this code
+            else
+                redirect_to login_path #TODO add flash message that login was denied
+            end
         else
-            redirect_to login_path #TODO add flash message that login was denied
+            if @employee && @employee.authenticate(params[:employee][:password])
+                redirect_to employee_path(@employee)
+            else
+                redirect_to employee_path(@curr_user) #TODO: add flash message that new employee was not created
+            end
         end
+
     end
 
     def show
