1f61afb6e14c2f00c72e9f2f65ffa19fca37eaa5
danschoch <daniel.alexander.schoch@gmail.com>
2018-12-27 07:46:31 -0800

    reset db with added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,13 @@
 #Organizations
 
 Organization.create(name: "Trump Organization")
+Organization.create(name: "Campaign")
 
 #Projects
 
 Project.create(name: "Trump Tower Moscow", due_date: DateTime.new.at_midnight, lead_id: 2, description: "corrupt new fancy tower", organization_id: 1)
-Project.create(name: "Campaign", due_date: DateTime.new.at_noon, lead_id: 2, description: "corrupt new fancy tower", organization_id: 1)
+Project.create(name: "Campaign", due_date: DateTime.new.at_noon, lead_id: 3, description: "corrupt new fancy tower", organization_id: 1)
+Project.create(name: "MAGA", due_date: DateTime.new.at_noon, lead_id: 4, description: "Made it pretty self-explanatory, didn't I?", organization_id: 2)
 
 
 #Employees
@@ -21,11 +23,13 @@ Project.create(name: "Campaign", due_date: DateTime.new.at_noon, lead_id: 2, des
 emp1 = Employee.new(first_name: "Eric", last_name: "Cartman", title: "Developer", lead: false, email: "lame@gmail.com", password_digest: BCrypt::Password.create("password"), organization_id: 1)
 emp2 = Employee.new(first_name: "Ivanna", last_name: "Thump", title: "Wannabe", lead: false, email: "bogus@fake.com", password_digest: BCrypt::Password.create("password"), organization_id: 1)
 emp3 = Employee.new(first_name: "Don", last_name: "Julio", title: "Junior Dev", lead: true, email: "extralame@yahoo.com", password_digest: BCrypt::Password.create("password"), organization_id: 1)
+emp4= Employee.new(first_name: "Paul", last_name: "Boyabase", title: "Chairman", lead: true, email: "iheartrussia@gmail.com", password_digest: BCrypt::Password.create("password"), organization_id: 2)
 
 
 emp1.save
 emp2.save
 emp3.save
+emp4.save
 
 #Tasks
 
