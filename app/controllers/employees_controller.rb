9a79eb56fdd45c72ce0334b94450872ec9213dab
danschoch <daniel.alexander.schoch@gmail.com>
2018-12-27 20:28:20 -0800

    clean up comments/messy code

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,5 @@
 class EmployeesController < ApplicationController
-    before_action :require_login, only: [:index, :show, :edit, :update, :destroy]#TODO Find a solution to this workaround
+    before_action :require_login, only: [:index, :show, :edit, :update, :destroy] #TODO Find a solution to this workaround
     before_action :set_employee, only: [:show, :edit, :update, :destroy]
     before_action :set_user
 
